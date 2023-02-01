35860bde4124714b73addd0a0aef20a0c5f94714
Rosemary Hoffman <123026684+Rosemaryhl@users.noreply.github.com>
2023-02-01 15:59:57 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,8 @@
+#Rosemary Hoffman
+import math
+x = math.pi/3
+y= math.pi/4
+numerator = math.sqrt(math.tan(y) +1)
+denom = math.sqrt(math.tan(x) + math.sin(x))
+value = numerator/denom
+print (value)
---
+++ L4.py
@@ -0,0 +1,7 @@
+#Rosemary Hoffman
+import math
+a = 4/math.sqrt(2)
+square = (4/math.sqrt(2))**2
+circle = math.pi * (4**2)
+value = circle - square
+print (value)
